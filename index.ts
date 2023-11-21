c57c90d5c6382fc988b176d072f1936095e30ba2
Tibet Tornaci <oofdere@hey.com>
2023-11-21 15:21:06 -0800

    move to /src

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
-export { Result, Ok, Err } from "./result";
-export { Option, Some, None } from "./option";
+export { Result, Ok, Err } from "./src/result";
+export { Option, Some, None } from "./src/option";
