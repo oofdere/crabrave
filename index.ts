18bbc460875e68641ed02c3dac5364f8a1760ddc
Tibet Tornaci <oofdere@hey.com>
2023-11-23 21:47:02 -0800

    remove fetch wrapper
    will be implemented again in the future

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export { Result, Ok, Err } from "./src/result";
-export { Option, Some, None } from "./src/option";
-export { safeFetch } from "./src/fetch";
+export { type Option, Some, None } from "./src/option";
 export { type Enum, pack, match } from "./src/enum";
