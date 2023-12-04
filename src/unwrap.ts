65854c05bd63390eb070e924798fd06bd2854b03
Tibet Tornaci <oofdere@hey.com>
2023-12-04 00:27:00 -0800

    and yes, even unwrap has been solved

---
+++ src/unwrap.ts
@@ -0,0 +1,17 @@
+// this adds the unwrap function to array prototypes
+
+import { Enum } from "./enum"
+import { Option } from "./option"
+import { Result } from "./result"
+
+declare global {
+    interface Array<T> {
+        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+        unwrap(this: Enum<Option<T>> | Enum<Result<T, any>>): T
+    }
+}
+
+Array.prototype.unwrap = function () {
+    if (this[0] === "Some" || this[0] === "Ok") { return this[1] }
+    throw TypeError(`${this[0]}(): ${this[1]}`)
+}
