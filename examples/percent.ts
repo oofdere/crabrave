1184c0129ab36478e4bc026b7cf041221a2b7af7
Tibet Tornaci <oofdere@hey.com>
2023-12-04 16:39:33 -0800

    update result guide

--- examples/percent.ts
+++ examples/percent.ts
@@ -6,27 +6,19 @@ type PercentError = {
     NegativeResult: number
 }
 
-function percent(n: number, total: number): Result<number, Enum<PercentError>> {
+function percent(n: number, total: number): Enum<Result<string, Enum<PercentError>>> {
     const p = n / total * 100;
 
     if (total < n) return Err(pack("TotalLessThanN", p));
     if (total === 0) return Err(pack("DivByZero", p));
     if (p < 0) return Err(pack("NegativeResult", p));
 
-    return Ok(p);
-}
-
-function stringify_percent(res: Result<string, Enum<PercentError>>) {
-    match(res, {
-        Ok: (x) => `${x}%`,
-        Err: () => "Something went wrong."
-    })
+    return Ok(`${p}%`);
 }
 
 const half = percent(1, 2);
-const half_str = stringify_percent(half);
-console.log(half_str);
+console.log(half.or("0%"))
+
 
 const double = percent(2, 1);
-const double_str = stringify_percent(half);
-console.log(double_str);
+console.log(double.or("0%"))
