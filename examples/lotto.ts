1a77410992dfe8c35a896a8bc3ac8051cab9bbd5
Tibet Tornaci <oofdere@hey.com>
2023-12-05 17:59:31 -0800

    [docs] option guide

---
+++ examples/lotto.ts
@@ -0,0 +1,40 @@
+import { Enum, None, Option, Some, match } from "..";
+
+let pool = 0;
+const chance = 0.1;
+const games = 10000;
+
+function play(): Enum<Option<number>> {
+    const win = Math.random() <= chance;
+    pool += 1;
+
+    if (win) {
+        const winnings = pool;
+        pool = 0;
+
+        return Some(winnings)
+    } else {
+        return None()
+    }
+}
+
+let balance = 0;
+
+for (const i of [...Array(games).keys()]) {
+    console.log(play())
+
+    balance += match(play(), {
+        Some: (x) => x, //=>
+        None: (x) => -1 //=>
+    })
+}
+
+console.log(`Net balance: $${balance}`);
+
+balance = 0;
+
+for (const i of [...Array(games).keys()]) {
+    balance += play().or(-1); //=>
+}
+
+console.log(`Net balance: $${balance}`);
