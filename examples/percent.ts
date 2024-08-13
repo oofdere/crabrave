605d85e4a689f2d0cf729c398c3d8df08eae6427
Tibet Tornaci <oofdere@hey.com>
2024-08-13 14:55:03 -0700

    Results

--- examples/percent.ts
+++ examples/percent.ts
@@ -1,4 +1,4 @@
-import { Ok, Err, Result, Enum, pack, match } from "../index";
+import { Ok, Err, type Result, pack } from "../index";
 
 type PercentError = {
     DivByZero: number,
@@ -6,7 +6,7 @@ type PercentError = {
     NegativeResult: number
 }
 
-function percent(n: number, total: number): Enum<Result<string, Enum<PercentError>>> {
+function percent(n: number, total: number): Result<string, PercentError> {
     const p = n / total * 100;
 
     if (total < n) return Err(pack("TotalLessThanN", p));
@@ -21,4 +21,4 @@ console.log(half.or("0%"))
 
 
 const double = percent(2, 1);
-console.log(double.or("0%"))
+console.log(double.or("over 100%"))
--- src/result.ts
+++ src/result.ts
@@ -1,7 +1,10 @@
 import type { Enum } from "./enum";
 import "./unwrap";
 
-export type Result<T, E> = ["Ok", T] | ["Err", E]
+export type Result<T, E> = {
+    Ok: T,
+    Err: E
+}
 
-export const Ok = <T, E>(ok: T, _err?: E): Result<T, E> => ["Ok", ok];
-export const Err = <T, E>(err: E, _ok?: T): Result<T, E> => ["Err", err];
+export const Ok = <T, E>(ok: T, _err?: E): Result<T, E> => ["Ok", ok] as unknown as Result<T, E>;
+export const Err = <T, E>(err: E, _ok?: T): Result<T, E> => ["Err", err] as unknown as Result<T, E>;
