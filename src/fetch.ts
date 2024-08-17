a8b265fead8965a22b1d5eff316d19e6ceb0cb03
Tibet Tornaci <oofdere@hey.com>
2024-08-16 23:18:50 -0700

    shave almost 100 bytes lmao

--- src/fetch.ts
+++ src/fetch.ts
@@ -2,50 +2,41 @@ import { Enum, match, pack } from "./enum";
 import { Err, Ok, type Result } from "./result";
 
 type FetchErr = {
-	AbortError: DOMException;
-	NotAllowedError: DOMException;
-	TypeError: TypeError;
-	Redirect: Response;
-	ClientError: Response;
-	ServerError: Response;
+    AbortError: DOMException;
+    NotAllowedError: DOMException;
+    TypeError: TypeError;
+    Redirect: Response;
+    ClientError: Response;
+    ServerError: Response;
 };
 
 const todo = (x?: unknown) => console.trace("todo!", x);
 const FetchErr = Enum<FetchErr>();
 
 export async function f(
-	input: string | URL | globalThis.Request,
-	init?: RequestInit,
+    input: string | URL | globalThis.Request,
+    init?: RequestInit,
 ): Promise<Result<Response, FetchErr>> {
-	try {
-		const res = await fetch(input, init);
+    try {
+        const res = await fetch(input, init);
 
-		if (res.ok) {
-			return Ok(res);
-		}
+        return res.ok
+            ? Ok(res)
+            : res.status < 400
+                ? Err(FetchErr("Redirect", res))
+                : res.status < 500
+                    ? Err(FetchErr("ClientError", res))
+                    : Err(FetchErr("ServerError", res));
 
-		if (res.status >= 300 && res.status < 400) {
-			return Err(FetchErr("Redirect", res));
-		}
+    } catch (e) {
+        if (e instanceof TypeError) {
+            return Err(FetchErr("TypeError", e));
+        }
 
-		if (res.status >= 400 && res.status < 500) {
-			return Err(FetchErr("ClientError", res));
-		}
+        if (e instanceof DOMException) {
+            return Err(FetchErr("AbortError", e));
+        }
 
-		if (res.status >= 500) {
-			return Err(FetchErr("ServerError", res));
-		}
-
-		return Ok(res);
-	} catch (e) {
-		if (e instanceof TypeError) {
-			return Err(FetchErr("TypeError", e));
-		}
-
-		if (e instanceof DOMException) {
-			return Err(FetchErr("AbortError", e));
-		}
-
-		throw e;
-	}
+        throw e;
+    }
 }
